a2fba49b5f48877af980c01f7a0aa3982a3823e6
PhilPalmer <phil.palmer.email@gmail.com>
2019-08-18 17:13:33 +0100

    Added my data to resumeData.js

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,104 +1,109 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "imagebaseurl":"https://philpalmer.github.io",
+    "name": "Phil Palmer",
+    "role": "Bioinformatician",
+    "linkedinId":"phil-palmer",
+    "skypeid": "phil_palmer_",
+    "roleDescription": "I am a bioinformatician who wants to use my career to do as much good as possible. I think I can do this by leveraging technologies such as bioinformatics & data science to help prevent disease.",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/phil-palmer/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"https://github.com/PhilPalmer",
           "className":"fa fa-github"
         },
         {
           "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
+          "url":"http://twitter.com/phil_palmer_",
           "className":"fa fa-twitter"
         }
       ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
+    "aboutme":"I am currently working at the biotech start-up Lifebit. Prior to this I graduated from the University of Southampton with a first class degree in Biology.",
+    // "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"London",
+    "website":"https://philpalmer.github.io",
     "education":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
+        "UniversityName":"University of Southampton",
+        "specialization":"BSc Biology",
+        "grade":"First Class Honours",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2018",
+        "Achievements":"First Class Honours.\nThesis: Differences in Telomere Length Predicted from the Exome Data of Bladder Cancer and Matched Control Tissue Samples 86%\nNotable modules include:\nBIOL3063 Bioinformatics & Systems Biology 81%\nBIOL2008 Quantitative Methods in Biological & Environmental Science 72%\nBIOL1005 Cell Biology & Genetics 70%"
       },
       {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "UniversityName":"Caterham School",
+        "specialization":"A-levels",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2015",
+        "Achievements":"Biology B, Business Studies B, Physics C, Mathematics (AS-level) B"
+      },
+      {
+        "UniversityName":"Caterham School",
+        "specialization":"IGCSEs",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2013",
+        "Achievements":"10 IGCSEs A*-B Including Mathematics and English"
       }
     ],
     "work":[
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"Lifebit",
+        "specialization":"Bioinformatician",
+        "MonthOfLeaving":"-",
+        "YearOfLeaving":"Present",
+        "Achievements":"Developing, maintaining & documenting bioinformatics pipelines on the Deploit platform. Have developed Docker & Nextflow pipelines on everything from variant calling to long reads to transcriptomics. For example, adapted a pipeline based on Google’s DeepVariant pipeline to meet the requirements of the open-source community nf-core. Also, regularly interact with clients & have been instrumental in the delivery of numerous key projects."
       }
     ],
     "skillsDescription":"Your skills here",
     "skills":[
       {
-        "skillname":"HTML5"
+        "skillname":"Bioinformatics"
       },
       {
-        "skillname":"CSS"
+        "skillname":"Python"
       },
       {
-        "skillname":"Reactjs"
+        "skillname":"R"
+      },
+      {
+        "skillname":"Bash"
       }
     ],
     "portfolio":[
       {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
+        "name":"nf-core/deepvariant",
+        "description":"Google's DeepVariant variant caller as a Nextflow pipeline",
+        "imgurl":"https://raw.githubusercontent.com/nf-core/deepvariant/master/docs/images/deepvariant_logo.png"
       },
       {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
+        "name":"Jackson Labs Workshop",
+        "description":"Prepared & taught a day long bioinformatics workshop to 45 participants in Jackson Labs in the US",
+        "imgurl":"images/portfolio/jax-workshop.jpg"
       },
       {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
+        "name":"RShiny Drug Repurposing App",
+        "description":"Built an RShiny application to demo to AstraZeneca for a drug repurposing pilot",  
+        "imgurl":"images/portfolio/az-dr-rshiny-app.png"
       },
       {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
+        "name":"Pharmacogenomics pipeline",
+        "description":"Pharmacogenomics Nextflow pipeline for the BHKLAB to process the GRAY 2017 dataset",
+        "imgurl":"https://avatars3.githubusercontent.com/u/12865570?s=280&v=4"
       }
     ],
     "testimonials":[
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
+        "description":"",
+        "name":""
       },
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
+        "description":"",
+        "name":""
       }
     ]
   }
