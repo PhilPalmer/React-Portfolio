a7d1cc4ea1f0a638f596c3962642228f8c026cca
PhilPalmer <phil.palmer.email@gmail.com>
2019-08-22 20:58:43 +0100

    Updates

---
+++ precache-manifest.078fd141b45903f608848224cf23e859.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd58c76427f5473b82627003e8bb5c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "135fd37a0dadadcf0bea",
+    "url": "/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "b2f363c36507fe11ff79",
+    "url": "/static/js/2.ac81b749.chunk.js"
+  },
+  {
+    "revision": "135fd37a0dadadcf0bea",
+    "url": "/static/js/main.03bfc880.chunk.js"
+  },
+  {
+    "revision": "7c23a43b52e067a1c7c0",
+    "url": "/static/js/runtime~main.f6dce284.js"
+  }
+]);
